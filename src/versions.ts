b52227c6c83cbb5f70930226c6eefe02cbaea016
CraigDeeming <craig.deeming@digital.cabinet-office.gov.uk>
2026-09-11 12:39:05 +0100

    fix: typos in publich config url

--- src/versions.ts
+++ src/versions.ts
@@ -73,7 +73,7 @@ export const PACKAGE_MANAGER = FILE.packageManager;
 
 /** Written to `publishConfig.registry` in every generated package.json */
 export const PUBLISH_CONFIG_REGISTRY =
-  'https://registry-prod-904690835784.d.codeartifact.eu-west-2.amazonaws.com/npm/registry-prod-repo/';
+  'https://registry-prod-904690835784.d.codeartifact.eu-west-2.amazonaws.com/npm/registry-prod-repo';
 
 /**
  * Toolchain owned by platform-pkg-dev itself. These are real `dependencies` of this
